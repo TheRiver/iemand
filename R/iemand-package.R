bd991c1123f870ea4bd82a1e1282997d51448fad
Constance Neeser <connie@brandseye.com>
2018-03-16 13:50:29 +0200

    Updated package documentation

--- R/iemand-package.R
+++ R/iemand-package.R
@@ -1,3 +1,10 @@
+#' Create random human data
+#'
+#' Create data. Nice data. The best of data.
+#'
+#' @details This is more detail. The best detail. Can't get enough detail.
+#'
+#' @author Constance Neeser
 #' @keywords internal
 #' @importFrom magrittr "%>%"
 "_PACKAGE"
